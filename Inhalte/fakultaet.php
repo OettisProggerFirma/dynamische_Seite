705a8ffaa566298959fa4a195d8dba47a110f12e
Denis Oetken <denisoetken@hotmail.de>
2016-06-06 09:32:19 +0200

    Test

---
+++ Inhalte/fakultaet.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Dynamische Webseite</title>
+    <link rel="stylesheet" href="../style.css">
+</head>
+<body>
+Lorem
+</body>
+</html>
---
+++ Inhalte/home.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Dynamische Webseite</title>
+    <link rel="stylesheet" href="../style.css">
+</head>
+<body>
+HOME
+<ul><li style="color: darkred">Test</li></ul>
+</body>
+</html>
